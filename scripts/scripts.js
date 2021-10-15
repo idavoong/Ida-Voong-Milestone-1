be9cad8c3f0ad438b93ad712dcbaf5f0f1986418
Ida Voong <idavoong@gmail.com>
2021-10-14 23:03:14 -0700

    changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,4 +7,28 @@ $(function () {
 
     //document.title = "your site name"
 
+
+
+    var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+    var alertTrigger = document.getElementById('liveAlertBtn')
+    
+    function alert(message, type) {
+      var wrapper = document.createElement('div')
+      wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+    
+      alertPlaceholder.append(wrapper)
+    }
+    
+    if (alertTrigger) {
+      alertTrigger.addEventListener('click', function () {
+        alert('Thank you for answering! Please click on the results', 'success')
+      })
+    }
+
+
+
+
   })
+
+
+  
